53ecfec8650e46ffabd68c4aac7a510e28c10cce
Akash1321 <Sarkarakash992@gmail.com>
2023-06-14 17:58:38 +0530

    refactor: code structure

--- src/Routes/AllRoutes.js
+++ src/Routes/AllRoutes.js
@@ -1,4 +1,4 @@
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import { Bookmarks, Explore, Home, Login } from "pages";
 import RequiresAuth from "./RequiresAuth";
@@ -6,14 +6,37 @@ import RequiresAuth from "./RequiresAuth";
 const AllRoutes = () => {
     return (
         <Routes>
-            <Route path="/" element={<RequiresAuth><Home/></RequiresAuth>}/>
-            <Route path="/explore" element={<RequiresAuth><Explore/></RequiresAuth>}/>
-            <Route path="/bookmarks" element={<RequiresAuth><Bookmarks/></RequiresAuth>}/>
-            <Route path="/login" element={<Login/>}/>
 
+            <Route
+                path="/"
+                element={
+                    <RequiresAuth>
+                        <Home />
+                    </RequiresAuth>
+                }
+            />
 
+            <Route
+                path="/explore"
+                element={
+                    <RequiresAuth>
+                        <Explore />
+                    </RequiresAuth>
+                }
+            />
+
+            <Route
+                path="/bookmarks"
+                element={
+                    <RequiresAuth>
+                        <Bookmarks />
+                    </RequiresAuth>
+                }
+            />
+            
+            <Route path="/login" element={<Login />} />
         </Routes>
-    )
-}
+    );
+};
 
 export default AllRoutes;
