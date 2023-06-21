fb54e38d5af1e2ba20753500cc47bf9fd1f8eadf
Akash1321 <Sarkarakash992@gmail.com>
2023-06-22 00:34:56 +0530

    feat: go to profile from bottom nav

--- src/components/bottomNav/BottomNav.js
+++ src/components/bottomNav/BottomNav.js
@@ -2,8 +2,13 @@ import { Home, Compass, Bookmark, Search, Plus } from "react-feather";
 import { NavLink } from "react-router-dom";
 
 import NavStyles from "./BottomNav.module.css";
+import { UserDp } from "components/userDp/UserDp";
+import { useAuth } from "context/AuthContext";
 
 const BottomNav = () => {
+
+  const {userInfo} = useAuth();
+  const {dpUrl, username} = userInfo;
 
     const styleActive = ({ isActive }) =>
       isActive ? `${NavStyles.active}` : "";
@@ -30,6 +35,12 @@ const BottomNav = () => {
           <li className={NavStyles.listItem}>
             <NavLink to="/bookmarks" className={styleActive}>
               <Bookmark className={NavStyles.icons} />
+            </NavLink>
+          </li>
+
+          <li className={NavStyles.listItem}>
+            <NavLink to="/profile" className={styleActive}>
+              <UserDp dpUrl={dpUrl} username={username} dimensions={NavStyles.dp} className={NavStyles.icons} />
             </NavLink>
           </li>
         </ul>
