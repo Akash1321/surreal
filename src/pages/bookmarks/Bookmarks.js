78ffd5b5d789fefd62665f4e514e55a7e2bee39d
Akash1321 <Sarkarakash992@gmail.com>
2023-07-02 12:49:40 +0530

    feat: bookmarked post list

--- src/pages/bookmarks/Bookmarks.js
+++ src/pages/bookmarks/Bookmarks.js
@@ -1,10 +1,20 @@
+import { Post } from "components";
+import { usePosts } from "context/PostsContext";
 
 
 const Bookmarks = () => {
+    const {state: {allPosts, allBookmarks}} = usePosts();
+
+    const bookmarkPosts = allPosts?.filter(({_id}) => allBookmarks?.includes(_id));
     
     return (
         <div className="content-container">
-            <h2>Bookmark</h2>
+            <h2>Bookmarks</h2>
+            <ul className="flex-container feed">
+                {bookmarkPosts?.map(postData => (
+                    <Post key={postData._id} {...postData} />
+                ))}
+            </ul>
         
         </div>
         
