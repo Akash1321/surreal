55bc72bcd5edc25fb5f4882f2c554bf0e94ad312
Akash1321 <Sarkarakash992@gmail.com>
2023-07-08 11:22:38 +0530

    feat: closing search tab and scroll top on location change

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import "./App.css";
 import { BottomNav, CreatePost, Sidebar, Search, Suggestion } from "components";
 import { useAuth } from "context/AuthContext";
 import AllRoutes from "Routes/AllRoutes";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import {useLocation} from "react-router-dom";
 
 function App() {
   const [showCreatePost, setShowCreatePost] = useState(false);
   const [showSearch, setShowSearch] = useState(false);
 
   const { token } = useAuth();
+  const location = useLocation();
+
+  const onLocationChange = () =>{
+    setShowSearch(false);
+    window.scroll(0, 0);
+  }
+
+  useEffect(() => {
+    onLocationChange()
+  }, [location]);
+
   return (
     <div className="App">
       {token && (
